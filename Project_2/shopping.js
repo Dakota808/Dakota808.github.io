95eb7627c615fe120df316b6e2a6f64f098cd1d5
Dakota_Koki <dkoki14@mail.wou.edu>
2018-10-03 12:32:26 -0700

    fixed html page and working on the javascript

---
+++ Project_2/shopping.js
@@ -0,0 +1,21 @@
+var item1;
+var item2;
+var item3;
+var quanity;
+var x;
+
+function calculation(){
+    if(x = item1){
+        x = item1 * quanity;
+        return x;
+    }else if (x = item2){
+        x = item2 * quanity;
+        return x;
+    }else if (x = item3){
+        x =item3 * quanity;
+        return x;
+    }else{
+        return "please input how many you are trying to purchase."
+    }
+   
+}
