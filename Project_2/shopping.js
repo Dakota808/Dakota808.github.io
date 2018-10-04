a6d4f7a9db3c53ad1ee30b9db3e600c763deb8c3
Dakota_Koki <dkoki14@mail.wou.edu>
2018-10-04 12:14:45 -0700

    Finalizing the javascript

--- Project_2/shopping.js
+++ Project_2/shopping.js
@@ -1,16 +1,35 @@
-var item1 = $("#item1").val();
-var item2 = $("#item2").val();
-var item3 = $("#item3").val();
 
-
-var itemOne = 3.50
-var itemTwo = 7.99
-var itemThree = 8.99
 
 function calculation(){
-   var subtotal = item1 * itemOne;
-   var subtotal = item2 * itemTwo;
-   var subtotal = item3 * itemThree;
+    var item1 = $("#item1").val();
+    var itemOne = 3.50;
+    var subtotal = item1 * itemOne;
+
    console.log("sub total = " + subtotal);
+   $('#tableIn').append( "<tr><td>Item #1</td><td>$3.50</td><td></td><td>$"+subtotal+"</td></tr>");
+   console.log(subtotal);
+}
+
+function calculation2(){
+    var item2 = $("#item2").val();
+    var itemTwo = 7.99;
+    var subtotal = item2 * itemTwo;
+    console.log("sub total = " + subtotal);
+    $('#tableIn').append( "<tr><td>Item #2</td><td>$7.99</td><td></td><td>$"+subtotal+"</td></tr>");
+    console.log(subtotal);
+
+}
+
+function calculation3(){
+    var item3 = $("#item3").val();
+
+    var itemThree = 8.99;
+
+    var subtotal = item3 * itemThree;
+    console.log("sub total = " + subtotal)
+    $('#tableIn').append( "<tr><td>Item #3</td><td>$8.99</td><td></td><td>$"+subtotal+"</td></tr>");
+    console.log(subtotal);
 }
 calculation();
+calculation2();
+calculation3();
