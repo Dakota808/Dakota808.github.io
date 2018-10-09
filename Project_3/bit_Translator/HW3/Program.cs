9e5544f7bc78b06b92416ef9cddb2821c7e94dd9
Dakota_Koki <dkoki14@mail.wou.edu>
2018-10-09 12:02:56 -0700

    added the project 3 and started on homework 3

---
+++ Project_3/bit_Translator/HW3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    class Node<T>
+    {
+        /// <summary>
+        /// This is in initalizing the the files data points in the nodes
+        /// </summary>
+        public T data;
+        public Node<T> next;
+
+        public Node(T data, Node<T> next)
+        {
+            ///This is the moving the pointer of the node 
+            ///to the next node in the data.
+            this.data = data;
+            this.next = next;
+        }
+
+        interface LinkedQueue<T> {
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
