a64b9e10a9dc26e38c4f539cbd54174099d3616e
Dakota_Koki <dkoki14@mail.wou.edu>
2018-10-10 14:11:19 -0700

    Implemented the QueueInterface file, now working to solve the LinkedQueue.

--- Project_3/bit_Translator/HW3/Program.cs
+++ Project_3/bit_Translator/HW3/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace HW3
 {
     class Node<T>
@@ -11,18 +12,69 @@ namespace HW3
         /// <summary>
         /// This is in initalizing the the files data points in the nodes
         /// </summary>
-        public T data;
-        public Node<T> next;
+        public T Data;
+        public Node<T> Next;
 
         public Node(T data, Node<T> next)
         {
             ///This is the moving the pointer of the node 
             ///to the next node in the data.
-            this.data = data;
-            this.next = next;
+            this.Data = data;
+            this.Next = next;
         }
 
-        interface LinkedQueue<T> {
+         interface IQueueInterface<T> {
+            /// <summary>
+            /// This is holidng all the elements and then from there it will add
+            /// the elements to the query. This will place that element to the end
+            /// of the query.
+            /// </summary>
+            /// <param name="element"></param>
+            /// <returns></returns>
+            T  Push(T element);
+
+            /// <summary>
+            /// This will take the top element from the top of the stack
+            /// Then will then return that element.
+            /// </summary>
+            /// <returns></returns>
+            T Pop();
+
+            /// <summary>
+            /// This is to check and see if the queue is empty.
+            /// This will return false if the queue is populated,
+            /// otherwise this will return true. 
+            /// </summary>
+            /// <returns></returns>
+            bool IsEmpty();
+
+        }
+
+        /// <summary>
+        /// This is using the idea of running a test to check for 
+        /// any operations that were executed/performed to a empty queue. 
+        /// </summary>
+        public class QueueUnderflowException : Exception
+        {
+            public QueueUnderflowException()
+            {
+                
+            }
+            /// <summary>
+            /// This will return a string message that will 
+            /// inform a illegal operation that is performed in the program.
+            /// </summary>
+            /// <param name="message"></param>
+            public QueueUnderflowException(string message) : base(message){
+
+
+
+            }
+
+        }
+
+        public class LinkedQueue : IQueueInterface<T> {
+
 
         }
         static void Main(string[] args)
