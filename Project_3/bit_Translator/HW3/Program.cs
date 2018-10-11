25d98391e38f607c903b87f1e89cafa3ce6fa02f
Dakota_Koki <dkoki14@mail.wou.edu>
2018-10-11 12:42:18 -0700

    polishing the HW3 file

--- Project_3/bit_Translator/HW3/Program.cs
+++ Project_3/bit_Translator/HW3/Program.cs
@@ -22,8 +22,9 @@ namespace HW3
             this.Data = data;
             this.Next = next;
         }
+    }
 
-         interface IQueueInterface<T> {
+    interface IQueueInterface<T> {
             /// <summary>
             /// This is holidng all the elements and then from there it will add
             /// the elements to the query. This will place that element to the end
@@ -31,7 +32,7 @@ namespace HW3
             /// </summary>
             /// <param name="element"></param>
             /// <returns></returns>
-            T  Push(T element);
+            T Push(T element);
 
             /// <summary>
             /// This will take the top element from the top of the stack
@@ -58,14 +59,14 @@ namespace HW3
         {
             public QueueUnderflowException()
             {
-                
+
             }
             /// <summary>
             /// This will return a string message that will 
             /// inform a illegal operation that is performed in the program.
             /// </summary>
             /// <param name="message"></param>
-            public QueueUnderflowException(string message) : base(message){
+            public QueueUnderflowException(string message) : base(message) {
 
 
 
@@ -77,7 +78,8 @@ namespace HW3
         /// <summary>
         /// 
         /// </summary>
-        public class LinkedQueue<T> : IQueueInterface<T> {
+        public class LinkedQueue<T> : IQueueInterface<T>
+        {
             private Node<T> front;
             private Node<T> rear;
 
@@ -102,29 +104,161 @@ namespace HW3
 
                 if (IsEmpty())
                 {
-                    Node<T> temp = new Node<T>()
+                    Node<T> temp = new Node<T>(element, null);
+                    rear = front = temp;
+
+                }
+                else
+                {
+                    Node<T> temp = new Node<T>(element, null);
+                    rear.Next = temp;
+                    rear = temp;
                 }
 
                 return element;
             }
 
-
-            public T Pop(T element)
+            /// <summary>
+            /// This is desgined to pull out the elements from the queue and removes it.
+            /// </summary>
+            /// <param name="element"></param>
+            /// <returns></returns>
+            public T Pop()
             {
-                throw new NotImplementedException();
+                T temp = default(T);
+
+                if (IsEmpty())
+                {
+                    throw new QueueUnderflowException("The Queue is empty when pop is invoked ");
+                }
+                else if (front == rear)
+                {
+                    /// This is when one item is in the Queue
+                    temp = front.Data;
+                    front = null;
+                    rear = null;
+                }
+                else
+                {
+                    /// This is all of the general cases
+                    temp = front.Data;
+                    front = front.Next;
+
+                }
+                return temp;
             }
 
 
             public bool IsEmpty()
             {
-                throw new NotImplementedException();
+                if (front == null && rear == null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
 
 
 
         }
+
+     /// <summary>
+     /// This will print the binary repesentation of all numbers from 1 up to n
+     /// By doing this it will design a virtual binary tree that will perform
+     /// a level order traversal using the FIFO queue. 
+     /// Result should be like this:
+     ///        1
+     ///    /       \
+     ///   10       11
+     ///  /  \     /  \
+     ///100  101 110  111
+     ///
+     /// etc.
+     /// which will store each value that was visited in the list.
+     /// </summary>
+        public class MainRun
+        {
+            static LinkedList<string> generateBinaryRepList(int n)
+            {
+                ///Creates an empty queue of strings
+                LinkedQueue<StringBuilder> x = new LinkedQueue<StringBuilder>();
+                ///The list is returning the binary values
+                LinkedList<string> output = new LinkedList<string>();
+
+                if (n < 1)
+                {
+                    /// binary values and representation do not support negative values
+                    /// return an empty list
+                    return output;
+                }
+
+                ///This will add the first binary number. This uses a dynamic string
+                ///to avoid string concat.
+                x.Push(new StringBuilder("1"));
+
+                ///BFS
+                while(n > 0)
+                {
+                    ///print the first part of the queue
+                    StringBuilder sb = x.Pop();
+                    output.AddLast(sb.ToString());
+
+                    ///Make a copy
+                    StringBuilder sbc = new StringBuilder(sb.ToString());
+
+                    ///Left Child
+                    sb.Append("0");
+                    x.Push(sb);
+                    ///Right Child
+                    sbc.Append("1");
+                    x.Push(sbc);
+                    n--;
+
+                }
+            return output;
+
+            }
+        /// <summary>
+        /// This will be the driver and is testing the program.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            int n = 10;
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Please invoke with the max value to print binary up to, like this:");
+                Console.WriteLine("\t Main 12");
+                return;
+            }
+            try
+            {
+                n = Int32.Parse(args[0]);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("I'm sorry, I can not understand the input: " + args[0]);
+            }
+
+            LinkedList<string> output = generateBinaryRepList(n);
+
+            int maxLength = output.Count();
+            foreach (string s in output)
+            {
+                for (int i = 0; i < maxLength - s.Length; i++)
+                {
+                    Console.WriteLine(" ");
+                }
+                Console.WriteLine(s);
+            }
         }
+
     }
+
+
+       
+
 }
