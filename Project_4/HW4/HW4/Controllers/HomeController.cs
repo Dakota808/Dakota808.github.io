990fa3c97875d3858447cdad1138281fbcf81d30
Dakota_Koki <dkoki14@mail.wou.edu>
2018-10-17 15:56:24 -0700

    finished the converter and now adding the viewbag() function in the controller

--- Project_4/HW4/HW4/Controllers/HomeController.cs
+++ Project_4/HW4/HW4/Controllers/HomeController.cs
@@ -14,9 +14,36 @@ namespace HW4.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Converter()
         {
             ViewBag.Message = "Miles to Metric Converter";
+            double miles = Convert.ToDouble(Request.QueryString["mile"]);
+            string mearsurement = Request.QueryString["metric-unit"];
+            Debug.WriteLine(miles);
+            Debug.WriteLine(mearsurement);
+
+            double result;
+            switch (mearsurement)
+            {
+                case "millimeters":
+                    result = miles * 1609344;
+                    break;
+                case "centimeters":
+                    result = miles * 160934.4;
+                    break;
+                case "meters":
+                    result = miles * 1609.344;
+                    break;
+                case "kilometers":
+                    result = miles * 1.609344;
+                    break;
+                default:
+                    result = -1;
+                    break;
+            }
+
+            ViewBag.Message();
 
             return View();
         }
@@ -28,7 +55,7 @@ namespace HW4.Controllers
             return View();
         }
 
-        [HttpGet]
+      
         public ActionResult Result()
         {
             {
