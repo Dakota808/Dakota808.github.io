eb74694c4c64e539f50b011a0827ae6838133502
Dakota_Koki <dkoki14@mail.wou.edu>
2018-10-17 14:43:01 -0700

    designing the converter form and about to work on the converter function

---
+++ Project_4/HW4/HW4/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Converter()
+        {
+            ViewBag.Message = "Miles to Metric Converter";
+
+            return View();
+        }
+
+        public ActionResult Color_Maker()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Result()
+        {
+            {
+                Debug.WriteLine(Request.QueryString["Input"]);
+
+                string name = Request.QueryString["user_name"];
+
+                return View();
+            }
+        }
+    }
+}
